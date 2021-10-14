9755c0bcdc4ba3644989f0847a67720bf39f2725
Kitsunejasutin <justineryandeguia@yahoo.com>
2021-10-14 18:21:53 +0800

    Experimental Image Slider

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const bgmodal = document.querySelector('.bg-modal');
 const modalBtn = document.getElementById('login');
 const exit = document.querySelector('.close');
 
+
 modalBtn.addEventListener('click', function() {
     bgmodal.style.display = 'flex';
 });
@@ -40,3 +41,58 @@ const navSlide = () => {
 }
 
 navSlide();
+
+const carouselSlide = document.querySelector('.carousel-slide');
+const carouselImages = document.querySelectorAll('.carousel-slide img');
+const prevBtn = document.querySelector('#prevBtn')
+const nextBtn = document.querySelector('#nextBtn')
+
+let counter = 1;
+const size = carouselImages[0].clientWidth;
+
+carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+nextBtn.addEventListener('click', () => {
+    if (counter >= carouselImages.length - 1) return;
+    carouselSlide.style.transition = "transform 0.4s ease-in-out";
+    counter++;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+prevBtn.addEventListener('click', () => {
+    if (counter <= 0) return;
+    carouselSlide.style.transition = "transform 0.4s ease-in-out";
+    counter--;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+});
+
+carouselSlide.addEventListener('transitionend', () => {
+    console.log(carouselImages[counter]);
+    if (carouselImages[counter].id === 'lastClone'){
+        carouselSlide.style.transition = "none";
+        counter = carouselImages.length -2;
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }  
+    if (carouselImages[counter].id === 'firstClone'){
+        carouselSlide.style.transition = "none";
+        counter = carouselImages.length - counter;
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }  
+});
+
+var nextSlide= function() {
+    if (counter > 0) {
+        carouselSlide.style.transition = "transform 0.4s ease-in-out";
+        counter++;
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }else if(counter < 0){
+        carouselSlide.style.transition = "transform 0.4s ease-in-out";
+        counter--;
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
+}
+
+setInterval(function(){
+    nextSlide()
+}, 5000);
