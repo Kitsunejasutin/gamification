4c55936f665bdfb170352dc82631abc34440c52d
Kitsunejasutin <justineryandeguia@yahoo.com>
2021-10-15 17:18:16 +0800

    Remove Panel and resolution improvements

--- js/script.js
+++ js/script.js
@@ -24,16 +24,6 @@ const navSlide = () => {
 
     //Toogle
     burger.addEventListener('click',()=>{
-        nav.classList.toggle('nav-active');
-
-        //Animation
-        navLinks.forEach((link, index) => {
-            if (link.style.animation){
-                link.style.animation = ''
-            }else{
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
-            }
-        });
         //Burger
         burger.classList.toggle('toggle');
     });
