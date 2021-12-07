2e72b7a08e1213057c4ecc2bafc02e1f051efda0
Kitsunejasutin <justineryandeguia@yahoo.com>
2021-12-07 09:09:32 +0800

    Commonized header

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
-<?php ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles/index.css">
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+<?php 
+    include_once 'includes/header.php';
+?>
     <title>Landing Page</title>
 </head>
 <body>
