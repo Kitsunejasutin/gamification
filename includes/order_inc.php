ab0c1cadf69090d982e71fe8a423d775b2d8c76e
Kitsunejasutin <justineryandeguia@yahoo.com>
2021-12-20 13:22:32 +0800

    Init Library System

--- includes/order_inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-    if (isset($_GET["success"])) {
-        if ($_GET["success"] == "added"){
-            echo "<span class='error'>Order Placed</span>";
-        }
-    }
--- index.php
+++ index.php
@@ -1,44 +1,111 @@
-<?php 
+<?php
     include_once 'includes/header.php';
     require_once 'includes/connection.php';
+    require_once 'includes/function.php';
+    session_start();
+    
 ?>
-    <link rel="stylesheet" href="styles/index.css">
-    <title>Landing Page</title>
+    <link rel="stylesheet" href="styles/header.css">
+    <link rel="stylesheet" href="styles/dashboard.css">
+	<title><?php echo fetchcompanyname($connection); ?></title>
 </head>
 <body>
-    <div class="container">
-        <div class="front-card">
-            <div class="card-header">
-                <div class="group-name">
-                    Welcome to the website
-                </div>
-                <h6 class="line-on-side text-muted font-small-3">
-                </h6>
-                <span>Employee LogIn</span>
-            </div>
-            <div class="card-body">
-                <form action="includes/login.php" method="POST" accept-charset="utf-8">
-                    <fieldset class="form-group fieldset-border">
-                        <i class="fas fa-user-circle"></i>
-                        <input type="text" class="card-control no-outline" name="email" placeholder="Your Email" required>
-                    </fieldset>
-                    <fieldset class="form-group fieldset-border">
-                        <i class="fas fa-key"></i>
-                        <input type="password" class="card-control no-outline" name="password" placeholder="Your Password" required>
-                    </fieldset>
-                    <div class="form-group row">
-                    <input type="checkbox"><div class="remember-me">Remember Me</div>
-                        <div class="forgot"><a href="forgot.php" class="forgot">Forgot Your Password?</a></div>
+<?php
+    include_once 'includes/bars.php';
+?>
+            <div class="info">
+                <div class="header">
+                    <div class="row">
+                        <div class="column first">
+                            <div class=""><i class="fas fa-cubes"></i><p class="text">Stocks</p></div>
+                            <p class="align-right"><?php 
+                            $column = "product_name";
+                            $table = "stocks";
+                            echo implode("|",countAll($connection, $column, $table)); ?></p>
+                        </div>
+                        <div class="column second">
+                            <div class=""><i class="fas fa-user-check"></i><p class="text">Employees</p></div>
+                            <p class="align-right"><?php 
+                            $column = "employee_id";
+                            $table = "accounts";
+                            echo implode("|",countAll($connection, $column, $table)); ?></p>
+                        </div>
+                        <div class="column third">
+                            <div class=""><i class="fas fa-parachute-box"></i><p class="text">Suppliers</p></div>
+                            <p class="align-right"><?php 
+                            $column = "supplier_name";
+                            $table = "supplier";
+                            echo implode("|",countAll($connection, $column, $table)); ?></p>
+                        </div>
                     </div>
-                    <?php include_once "includes/login_errors.php"; ?>
-                    <button class="btn grey" name="login"><span>LogIn</span></button>
-                </form>
-                <h6 class="line-on-side text-muted font-small-3">
-                </h6>
-                <span>Are you a Client?</span>
-                <button class="btn grey"><span>LogIn</span></button>
+                </div>
+                <div class="card">
+                    <div class="fourth">
+                        <p class="header">Time Clock</p>
+                        <table class="tableone">
+                            <thead>
+                                <th>Employee</th>
+                                <th>Status</th>
+                                <th>Time In</th>
+                                <th>Time Out</th>
+                            </thead>
+                            <?php
+                                $sql = "SELECT * FROM timeclock";
+                                $stmt = mysqli_stmt_init($connection);
+                                if (!mysqli_stmt_prepare($stmt, $sql)) {
+                                    header("location: ../index.php?error=stmtfailedexists");
+                                    exit();
+                                }
+                                mysqli_stmt_execute($stmt);
+
+                                $resultData = mysqli_stmt_get_result($stmt);
+
+                                while($data = mysqli_fetch_array($resultData)){
+                            ?>
+                                <tbody>
+                                    <tr class="one">
+                                        <th><?php echo $data[3]?></th>
+                                        <th><?php echo $data[2]?></th>
+                                        <th><?php echo $data[4]?></th>
+                                        <th><?php echo $data[5]?></th>
+                                    </tr>
+                                </tbody>
+                            <?php }mysqli_stmt_close($stmt); ?>
+                        </table>
+                    </div>
+                    <div class="fifth">
+                        <p class="header">Orders</p>
+                        <table class="tabletwo">
+                            <thead>
+                                <th>Product Name</th>
+                                <th>Quantity</th>
+                                <th>Price</th>
+                            </thead>
+                            <?php
+                                $sql = "SELECT * FROM order_list";
+                                $stmt = mysqli_stmt_init($connection);
+                                if (!mysqli_stmt_prepare($stmt, $sql)) {
+                                    header("location: ../index.php?error=stmtfailedexists");
+                                    exit();
+                                }
+                                mysqli_stmt_execute($stmt);
+
+                                $resultData = mysqli_stmt_get_result($stmt);
+
+                                while($data = mysqli_fetch_array($resultData)){
+                            ?>
+                                <tbody>
+                                    <tr>
+                                        <th><?php echo $data[2]?></th>
+                                        <th><?php echo $data[5]?></th>
+                                        <th><?php echo $data[6]?></th>
+                                    </tr>
+                                </tbody>
+                            <?php }mysqli_stmt_close($stmt); ?>
+                        </table>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-</body>
-</html>
+<?php 
+	include_once 'includes/footer.php';
+?>
