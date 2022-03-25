99d952688302d47358a76966c52358a327888ed5
Kitsunejasutin <73803767+Kitsunejasutin@users.noreply.github.com>
2022-03-26 07:21:30 +0800

    Only appears when a admin logins

--- includes/bars.php
+++ includes/bars.php
@@ -7,9 +7,15 @@
             <ul id="categories">
                 <li class="categories"><a><i class="fas fa-book-open"></i>Books<i class="arrow right"></i></a></li>
                 <a href="listbook.php"><li class="dropdown account"><span>List Books</span></li></a>
-                <a href="addbook.php"><li class="dropdown account"><span>Add Books</span></li></a>
-                <a href="borrowbook.php"><li class="dropdown account"><span>Borrow Book</span></li></a>
-                <a href="returnbook.php"><li class="dropdown account"><span>Return Book</span></li></a>
+                    <?php
+                        if(isset($_SESSION['name'])){ 
+                            ?>
+                                <a href="addbook.php"><li class="dropdown account"><span>Add Books</span></li></a>
+                                <a href="borrowbook.php"><li class="dropdown account"><span>Borrow Book</span></li></a>
+                                <a href="returnbook.php"><li class="dropdown account"><span>Return Book</span></li></a>
+                            <?php
+                        }
+                    ?>
             </ul>
             <ul id="categories">
                 <li class="categories"><a><i class="fas fa-list"></i><span>Categories</span><i class="arrow right"></i></a></li>
@@ -31,16 +37,30 @@
                     mysqli_stmt_close($stmt);
                 ?>
             </ul>
-            <ul id="categories">
-                <li class="categories"><a><i class="fas fa-user-tie"></i>Account<i class="arrow right"></i></a></li>
-                <a href="attendance.php"><li class="dropdown account"><span>Edit Account</span></li></a>
-                <a href="addstudent.php"><li class="dropdown account"><span>Add Student</span></li></a>
-                <?php if(isset($_SESSION['name'])){ echo '<a href="includes/logout.php"><li class="dropdown account"><span>LogOut</span></li></a>'; } ?>
-            </ul>
-            <ul id="categories">
-                <li class="categories"><a><i class="fas fa-table"></i>Data & Reports<i class="arrow right"></i></a></li>
-                <a href="transaction.php"><li class="dropdown"><span>Transaction History</span></li></a>
-            </ul>
+            <?php 
+                if(isset($_SESSION['name'])){ 
+                    ?>
+                    <ul id="categories">
+                        <li class="categories"><a><i class="fas fa-user-tie"></i>Account<i class="arrow right"></i></a></li>
+                        <a href="attendance.php"><li class="dropdown account"><span>Edit Account</span></li></a>
+                        <a href="addstudent.php"><li class="dropdown account"><span>Add Student</span></li></a>
+                        <?php if(isset($_SESSION['name'])){ echo '<a href="includes/logout.php"><li class="dropdown account"><span>LogOut</span></li></a>'; } ?>
+                    </ul>
+                <?php
+                }else{ 
+                    
+                } 
+            ?>
+            <?php
+                if(isset($_SESSION['name'])){ 
+                    ?>
+                        <ul id="categories">
+                            <li class="categories"><a><i class="fas fa-table"></i>Data & Reports<i class="arrow right"></i></a></li>
+                            <a href="transaction.php"><li class="dropdown"><span>Transaction History</span></li></a>
+                        </ul>
+                    <?php
+                }
+            ?>
         </div>
         <div class="main_content">
             <div class="topbar">
